75691949aebca9377467902593049326da8b5896
Jitesh596 <jtskumar05@gmail.com>
2026-08-20 11:12:27 +0530

    feat: add tour model and controller files

---
+++ model/tourModel.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const toursFilePath = path.join(__dirname, '../data/tours.json');
+
+const getAllTours = () => {
+  const toursData = fs.readFileSync(toursFilePath, 'utf-8');
+  return JSON.parse(toursData);
+}
+
+const getById = (id) => {
+  const tours = getAllTours();
+  return tours.find(tour => tour.id === id);
+}
+
+module.exports = {
+  getAllTours,
+  getById
+};
