7981c7f11dd0dfddfda7fa516194940a7adc771b
Jitesh596 <jtskumar05@gmail.com>
2026-08-21 21:51:35 +0530

    Update changes

--- model/tourModel.js
+++ model/tourModel.js
@@ -1,18 +1,44 @@
 const fs = require('fs');
 const path = require('path');
-const toursFilePath = path.join(__dirname, '../data/tours.json');
+const toursFilePath = path.join(__dirname, '../data/tour.json');
 
-const getAllTours = () => {
-  const toursData = fs.readFileSync(toursFilePath, 'utf-8');
-  return JSON.parse(toursData);
-}
+const getAll = () => {
+    const toursData = fs.readFileSync(toursFilePath, 'utf-8');
+    return JSON.parse(toursData);
+};
 
 const getById = (id) => {
-  const tours = getAllTours();
-  return tours.find(tour => tour.id === id);
+    const toursData = getAll();
+    return toursData.find(tour => tour.id === id);
 }
 
-module.exports = {
-  getAllTours,
-  getById
+const getByquery = (query) => {
+    const tours = getAll();
+    return tours.filter(tour => tour.name.includes(query));
+}
+
+const save = (tours) => {
+    const existingTours = getAll();
+    existingTours.push(tours);
+    fs.writeFileSync(toursFilePath, JSON.stringify(existingTours, null, 2),'utf-8');
+}
+
+const updateTour = (id, updatedTour) => {
+    const tours = getAll();
+    const index = tours.findIndex(tour => tour.id === id);
+    if (index !== -1) {
+        tours[index] = { ...tours[index], ...updatedTour };
+        fs.writeFileSync(toursFilePath, JSON.stringify(tours, null, 2), 'utf-8');
+    }
 };
+
+module.exports = {
+    getAll,
+    getById,
+    getByquery,
+    save,
+    updateTour
+};
+
+
+// index.js --> routes/tourRoutes.js --> controller/tourController.js --> model/tourModel.js
