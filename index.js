9351e28e4440f695f40135152be3d69096bfcb96
Jitesh596 <jtskumar05@gmail.com>
2026-09-03 11:26:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -73,11 +73,11 @@ const express = require("express");
 
 const app = express();
 
-const tourRoutes = require("./routes/tourRoutes");
+// const tourRoutes = require("./routes/tourRoutes");
 
 app.use(express.json());
 
-app.use("/api", tourRoutes);
+// app.use("/api", tourRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
